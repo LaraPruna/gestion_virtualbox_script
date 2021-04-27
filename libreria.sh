aad38827b4f058a1316a5eb874de229fe6aadff5
Lara Pruna Ternero <laraprn.12@gmail.com>
2021-04-27 16:04:42 +0200

    He modificado vboxmanager.sh y he añadido una nueva función a la librería

--- libreria.sh
+++ libreria.sh
@@ -12,6 +12,18 @@ function f_eres_root {
 	fi
 }
 
+#Mediante esta función, comprobamos la existencia de un directorio.
+#Devuelve un 0 si el directorio existe y 1 si no existe.
+#Acepta un argumento, que es el directorio que se quiera comprobar.
+function f_existe_directorio {
+        if [[ -d $1 ]]
+                then
+                        return 0
+                else
+                        return 1
+        fi
+}
+
 #Esta función comprueba si un paquete está instalado o no.
 #Acepta como argumento el nombre del paquete.
 #Devuelve 0 si está instalado y 1 si no lo está.
@@ -62,44 +74,107 @@ function f_instalar_virtualbox {
 	fi
 }
 
-#Esta función muestra información sobre las máquinas virtuales registradas. Mediante un menú guardado
+#Esta función muestra información general sobre las máquinas virtuales registradas. Mediante un menú guardado
 #en otro fichero y mostrado a través de esta función, el usuario podrá elegir el tipo de información
 #que busca en particular.
 #No acepta argumentos de entrada.
-function f_vminfo_ {
+function f_vmsinfo {
 	cat ./info.txt
 	read opcion
 	while [[ $opcion != 16 ]]; do
 		if [[ $opcion = 1 ]]; then
-			vboxmanage list vms
+			if [[ $(vboxmanage list vms | wc -l) = 0 ]]; then
+				echo 'No tienes ninguna máquina virtual registrada.'
+			else
+				echo 'Máquinas virtuales registradas:'
+				vboxmanage list vms
+			fi
 		elif [[ $opcion = 2 ]]; then
-                        vboxmanage list runningvms
+			if [[ $(vboxmanage list runningvms | wc -l) = 0 ]]; then
+				echo 'No se está ejecutando ninguna máquina virtual en este momento.'
+			else
+				echo 'Máquinas virtuales en ejecución:'
+        	                vboxmanage list runningvms
+			fi
 		elif [[ $opcion = 3 ]]; then
+			echo 'Sistemas operativos disponibles en Virtualbox:'
                         vboxmanage list ostypes
 		elif [[ $opcion = 4 ]]; then
-                        vboxmanage list intnets
+			if [[ $(vboxmanage list intnets | wc -l) = 0 ]]; then
+				echo 'No se ha encontrado ninguna red interna.'
+			else
+				echo 'Redes internas:'
+	                        vboxmanage list intnets
+			fi
 		elif [[ $opcion = 5 ]]; then
-                        vboxmanage list bridgedifs
+			if [[ $(vboxmanage list bridgedifs | wc -l) = 0 ]]; then
+				echo 'No se ha encontrado ninguna interfaz con adaptador puente disponible.'
+			else
+				echo 'Interfaces con adaptador puente:'
+	                        vboxmanage list bridgedifs
+			fi
 		elif [[ $opcion = 6 ]]; then
-                        vboxmanage list hostonlyifs
+			if [[ $(vboxmanage list hostonlyifs | wc -l) = 0 ]]; then
+                                echo 'No se ha encontrado ninguna interfaz de solo anfitrión disponible.'
+                        else
+                                echo 'Interfaces de solo anfitrión:'
+	                        vboxmanage list hostonlyifs
+			fi
 		elif [[ $opcion = 7 ]]; then
-                        vboxmanage list natnets
+			if [[ $(vboxmanage list natnets | wc -l) = 0 ]]; then
+                                echo 'No se ha encontrado ninguna interfaz con NAT.'
+                        else
+                                echo 'Interfaces con NAT:'
+	                        vboxmanage list natnets
+			fi
 		elif [[ $opcion = 8 ]]; then
-                        vboxmanage list dhcpservers
+			if [[ $(vboxmanage list dhcpservers | wc -l) = 0 ]]; then
+                                echo 'No se ha encontrado ningún servidor DHCP disponible.'
+                        else
+                                echo 'Servidores DHCP:'
+	                        vboxmanage list dhcpservers
+			fi
 		elif [[ $opcion = 9 ]]; then
+			echo 'Información sobre el host:'
                         vboxmanage list hostinfo
 		elif [[ $opcion = 10 ]]; then
-                        vboxmanage list hdds
+			if [[ $(vboxmanage list hdds | wc -l) = 0 ]]; then
+                                echo 'No estás utilizando ningún disco duro en este momento.'
+                        else
+                                echo 'Discos duros en uso:'
+	                        vboxmanage list hdds
+			fi
 		elif [[ $opcion = 11 ]]; then
-                        vboxmanage list dvds
+			if [[ $(vboxmanage list dvds | wc -l) = 0 ]]; then
+                                echo 'No estás utilizando ningún disco de DVD en este momento.'
+                        else
+                                echo 'Discos de DVD en uso:'
+	                        vboxmanage list dvds
+			fi
 		elif [[ $opcion = 12 ]]; then
-                        vboxmanage list usbhost
+			if [[ $(vboxmanage list usbhost | egrep '<none>') ]]; then
+                                echo 'No hay ningún dispositivo USB conectado al host en este momento.'
+                        else
+                                echo 'Dispositivos USB conectados al host:'
+	                        vboxmanage list usbhost
+			fi
 		elif [[ $opcion = 13 ]]; then
+			echo 'Configuración general de Virtualbox:'
                         vboxmanage list systemproperties
 		elif [[ $opcion = 14 ]]; then
-                        vboxmanage list extpacks
+			if [[ $(vboxmanage list extpacks | egrep 'Extension Packs: 0') ]]; then
+                                echo 'No tienes instalado ningún paquete de extensión en Virtualbox.'
+			else
+				echo 'Paquetes de extensión instalados:'
+	                        vboxmanage list extpacks
+			fi
 		elif [[ $opcion = 15 ]]; then
-                        vboxmanage list groups
+			if [[ $(vboxmanage list groups | wc -l) = 0 ]]; then
+                                echo 'No tienes ningún grupo de máquinas virtuales creado.'
+                        else
+                                echo 'Grupos de máquinas virtuales:'
+	                        vboxmanage list groups
+			fi
 		else
 			echo 'Error. Introduce una opción del menú.'
 		fi
@@ -108,4 +183,99 @@ function f_vminfo_ {
 	done
 }
 
+#Esta función muestra información sobre una máquina virtual en particular.
+#Acepta como argumento de entrada el nombre o la UUID de la máquina virtual.
+#Devuelve 0 al mostrar la información y 1 si no se encuentra la máquina.
+function f_vminfo {
+	if [[ $(vboxmanage showvminfo $1 &> /dev/null; echo $?) = 0 ]]; then
+		vboxmanage showvminfo $1
+	else
+		echo 'Esa máquina no se encuentra registrada.'
+	fi
+}
 
+#Esta función crea una máquina virtual a partir de la información introducida por el usuario
+#mediante un cuestionario.
+#No acepta argumentos de entrada.
+function f_crearvm {
+	echo '¿Cómo quieres llamar a la máquina?'
+	read nombre
+	echo 'Introduce la ruta del directorio donde quieres guardarla (por defecto, /home/usuario/VirtualBox VMs):'
+	read ruta
+	if [[ $ruta != $null ]]; then
+		while [[ $(cd $ruta;echo $?) = 1 ]]; do
+        	        echo 'Ese directorio no existe. Introduce otra ruta:'
+        	        read ruta
+                	if [[ $ruta = $null ]]; then
+	                	continue
+                	fi
+        	done
+	fi
+	while [[ $(cd $ruta;echo $?) = 1 ]]; do
+		echo 'Ese directorio no existe. Introduce otra ruta:'
+		read ruta
+		if [[ $ruta = $null ]]; then
+                	ruta="/home/usuario/VirtualBox\ VMs/"
+	        fi
+	done
+	echo 'Introduce el nombre del grupo que quieras asignarle a la máquina (por defecto "/"):'
+	read grupo
+	if [[ $grupo != $null ]]; then
+		while [[ $(vboxmanage list groups | egrep $grupo; echo $?) = 1 ]]; do
+			echo 'No hay ningún grupo con ese nombre. Introduce otro:'
+			read grupo
+			if [[ $grupo = $null ]]; then
+				continue
+			fi
+		done
+	fi
+	echo '¿Qué sistema operativo quieres asignarle a la máquina? (L para mostrar los sistemas operativos disponibles)'
+	read os
+	if [[ $os = 'L' ]]; then
+		vboxmanage list ostypes
+		echo '¿Qué sistema operativo quieres asignarle a la máquina? (L para mostrar los sistemas operativos disponibles)'
+	        read os
+	fi
+	while [[ $(vboxmanage list ostypes | egrep $os; echo $?) = 1 ]]; do
+		echo 'No hay ningún sistema operativo disponible con ese nombre.'
+		echo 'Introduce otro sistema operativo (L para mostrar los sistemas operativos disponibles):'
+		read os
+		if [[ $os = 'L' ]]; then
+			vboxmanage list ostypes
+		fi
+	done
+	echo '¿Quieres registrar la nueva máquina virtual en Virtualbox? (s/n)'
+	read reg
+	echo 'Creando máquina virtual...'
+	if [[ $reg = 's' ]]; then
+		if [[ $ruta = $null ]]; then
+			if [[ $grupo = $null ]]; then
+				vboxmanage createvm --name $nombre --basefolder /home/usuario/VirtualBox\ VMs/ --ostype $os --register
+			else
+				vboxmanage createvm --name $nombre --basefolder /home/usuario/VirtualBox\ VMs/ --group $grupo --ostype $os --register
+			fi
+		else
+			if [[ $grupo = $null ]]; then
+				vboxmanage createvm --name $nombre --basefolder $ruta --ostype $os --register
+			else
+				vboxmanage createvm --name $nombre --basefolder $ruta --group $grupo --ostype $os --register
+			fi
+                        echo 'Máquina virtual creada. Puedes configurarla desde el menú principal.'
+		fi
+	else
+		if [[ $ruta = $null ]]; then
+			if [[ $grupo = $null ]]; then
+				vboxmanage createvm --name $nombre --basefolder /home/usuario/VirtualBox\ VMs/ --ostype $os
+			else
+				vboxmanage createvm --name $nombre --basefolder /home/usuario/VirtualBox\ VMs/ --group $grupo --ostype $os
+			fi
+		else
+			if [[ $grupo = $null ]]; then
+				vboxmanage createvm --name $nombre --basefolder $ruta --ostype $os
+			else
+				vboxmanage createvm --name $nombre --basefolder $ruta --group $grupo --ostype $os
+			fi
+		fi
+		echo 'Máquina virtual creada. Puedes registrarla y configurarla desde el menú principal.'
+	fi
+}
--- vboxmanager.sh
+++ vboxmanager.sh
@@ -2,15 +2,19 @@
 
 . ./libreria.sh
 
-if [[ $(f_eres_root;echo $?) = 1 ]]; then
-	echo 'Tienes que ser root'
-	exit
+if [[ $(f_eres_root;echo $?) = 0 ]]; then
+        echo 'Ejecuta el programa con tu usuario.'
+        exit
 fi
+
 echo 'Inicio del programa. Comprobando el estado del paquete virtualbox...'
 if [[ $(f_esta_instalado virtualbox;echo $?) = 1 ]]; then
 	echo 'No tienes instalado Virtualbox. ¿Quieres instalarlo? (s/n)'
 	read res1
 	if [[ $res1 = 's' ]]; then
+		if [[ $(f_eres_root;echo $?) = 1 ]]; then
+			sudo su
+		fi
 		f_instalar_virtualbox
 		if [[ $(echo $?) = 3 ]]; then
 			exit
@@ -22,14 +26,17 @@ if [[ $(f_esta_instalado virtualbox;echo $?) = 1 ]]; then
 fi
 cat ./menu.txt
 read opcion
-while [[ $opcion > 0 && $opcion < 14 ]]; do
-	echo 'Error. Introduce una opción del menú.'
-	cat ./menu.txt
-	read opcion
-done
-while [[ $opcion != 13 ]]; do
+while [[ $opcion != 14 ]]; do
 	if [[ $opcion = 1 ]]; then
-		
+		f_vmsinfo
+	elif [[ $opcion = 2 ]]; then
+		echo 'Introduce el nombre de la máquina virtual:'
+		read mv1
+		f_vminfo $mv1
+	elif [[ $opcion = 3 ]]; then
+		f_crearvm
+	else
+		echo 'Error. Introduce una opción del menú.'
 	fi
 	cat ./menu.txt
 	read opcion
